eca574793d8e07c9da3ac36607f575c44b4f35b2
05remla <05remla@gmail.com>
2019-05-29 20:47:42 +0300

    Update kernel_install.sh

--- kernel_install.sh
+++ kernel_install.sh
@@ -1,29 +1,49 @@
 #!/bin/bash
-items=$(ls "${EXT_SQUASH_FS}/boot/init"*)
-if [[ $(echo $items | wc -w) == 1 ]]; then
-  #item1=($(ls "${EXT_SQUASH_FS}/boot/init"*))
-  #item2=($(ls "${EXT_SQUASH_FS}/boot/vmlinuz"*))
-  
-else
-  xx=1
-  versions=""
-  for i in $items
-  do
-    ver=$($i | cut -d- -f2)
-    versions="${versions} ${ver}"
-    echo "${xx}. ${ver}"
-  done
-  echo "which kernel will you use: "
-  read option
-  ver=$(echo $versions | cut -d" " -f${option})
+# echo "kernel directory?"
+# printf "> "
+# read option
 
-  cd "${EXT_SQUASH_FS}/boot"
-  files=$(ls *"${ver}"*)
+# kerns=($(ls "${option}/initrd"*))
+# if [[ -z ${kerns} ]]; then
+  # echo "no kernels in ${option}. exiting..."
+  # exit
+# fi
 
-rm -rf item1
-rm -rf item2
+# indx=0
+# versions=""
+# while [[ ! -z ${kerns[indx]} ]]; do
+  # ver=$(echo ${kerns[$indx]} | cut -d- -f2)
+  # versions="${versions} ${ver}"
+  # ((indx++))
+# done
+
+# indx=0
+# for i in $versions
+# do
+  # echo "${indx}. ${i}"
+  # ((indx++))
+# done
+# echo
+
+# echo "select kernel version"
+# printf "[#]> "
+# read option
+
+# version=($(echo $versions))
+# selected_kernel="${versions[$option]}"
+# echo "you selected ${selected_kernel}"
+# echo "proceed?"
+# read option
+
+# if [[ $option == "yes" ]]; then
+  # ls "/boot/init"*"${selected_kernel}"*
+  # ls "/boot/vmlinuz"*"${selected_kernel}"*
+# fi
+
+rm -rf "${EXT_SQUASH_FS}/boot/init"*
+rm -rf "${EXT_SQUASH_FS}/boot/vmlinuz"*
+
 mkdir -p "${EXT_SQUASH_FS}/kernel_install"
 cp -r "${SYS_BUILD_HOME}/kernel" "${EXT_SQUASH_FS}/kernel_install"
 chroot "${EXT_SQUASH_FS}" dpkg -i "/kernel_install/*.deb" | tee -a /var/log/kernel_install.log
 chroot "${EXT_SQUASH_FS}" update-initramfs -u
-chroot "${EXT_SQUASH_FS}"
